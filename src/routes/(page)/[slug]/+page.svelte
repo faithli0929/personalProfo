016191028ae94a0fce3db8ffd5fdb89e863499ec
Faith LI <128820090+faithli0929@users.noreply.github.com>
2024-07-07 11:13:06 +0800

    Fix

--- src/routes/(page)/[slug]/+page.svelte
+++ src/routes/(page)/[slug]/+page.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import Badge from '$lib/components/ui/badge/badge.svelte';
-	import { fade, fly } from 'svelte/transition';
-    import { backIn, backOut, backInOut } from 'svelte/easing'
 
     export let data;
     $:content = data.page[0]
